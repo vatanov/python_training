6d8c68f66d0fe04b69b34283040e956a19d37388
vatanov <atanov.v@gmail.com>
2016-10-03 18:03:01 +0300

    Changes from work

--- fixture/appliation.py
+++ fixture/appliation.py
@@ -1,20 +1,16 @@
 from selenium.webdriver.firefox.webdriver import WebDriver
-# from fixture.session import SessionHelper
-# from fixture.group import GroupHelper
-# from fixture.contact import ContactHelper
-from fixture.helper_manager import HelperManager
+from fixture.session import SessionHelper
+from fixture.group import GroupHelper
+from fixture.contact import ContactHelper
 
 
 class Application:
     def __init__(self):
         self.wd = WebDriver()
         self.wd.implicitly_wait(60)
-        # self.session = SessionHelper(self)
-        # self.group = GroupHelper(self)
-        # self.contact = ContactHelper(self)
-        self.session = HelperManager(self)
-        self.group = HelperManager(self)
-        self.contact = HelperManager(self)
+        self.session = SessionHelper(self)
+        self.group = GroupHelper(self)
+        self.contact = ContactHelper(self)
 
     def open_home_page(self):
         wd = self.wd
