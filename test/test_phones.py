948cbb5814508925f327ab0c8f21217a442380cb
Vladimir Atanov <atanov.v@gmail.com>
2016-10-23 10:56:04 +0300

    test_name added

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,8 +1,12 @@
 import re
+from random import randrange
+
 
 def test_phones_on_home_page(app):
-    contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contacts = app.contact.get_contact_list()
+    index = randrange(len(contacts))
+    contact_from_home_page = app.contact.get_contact_list()[index]
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_home_page.all_phones_from_home_page == merge_phones_like_on_home_page(contact_from_edit_page)
 
 
@@ -18,6 +22,9 @@ def test_phones_on_contact_view_page(app):
 def clear(s):
     return re.sub("[() -]", "", s)
 
+
 def merge_phones_like_on_home_page(contact):
     return "\n".join(filter(lambda x: x != "",
-                            map(lambda x: clear(x), [contact.home, contact.work, contact.mobile, contact.phone2])))
+                            map(lambda x: clear(x),
+                                filter(lambda x: x is not None,
+                                       [contact.home, contact.mobile, contact.work, contact.phone2]))))
