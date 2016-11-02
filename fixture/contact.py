eb2b787ea8f69f1ae1b5d70d92ca28dfd24231af
Vladimir Atanov <atanov.v@gmail.com>
2016-11-03 00:06:29 +0200

    Resolved some small issues

--- fixture/contact.py
+++ fixture/contact.py
@@ -90,16 +90,21 @@ class ContactHelper:
     contact_cache = None
 
     def get_contact_list(self):
-       if self.contact_cache is None:
+        if self.contact_cache is None:
             wd = self.app.wd
             self.app.open_home_page()
             self.contact_cache = []
             for element in wd.find_elements_by_name("entry"):
                 cells = element.find_elements_by_tag_name("td")
                 id = cells[0].find_element_by_tag_name("input").get_attribute("value")
+                firstname = cells[2].text
                 lastname = cells[1].text
-                self.contact_cache.append(Contact(id=id, lastname=lastname))
-       return self.contact_cache
+                all_phones = cells[5].text
+                all_emails = cells[4].text
+                self.contact_cache.append(Contact(id=id, firstname=firstname, lastname=lastname,
+                                                  all_phones_from_home_page=all_phones,
+                                                  all_emails_from_home_page=all_emails))
+        return self.contact_cache
 
     def open_contact_view_by_index(self, index):
         wd = self.app.wd
@@ -131,7 +136,7 @@ class ContactHelper:
         email3 = wd.find_element_by_name("email3").get_attribute("value")
         address = wd.find_element_by_name("address").text
         return Contact(firstname=firstname, lastname=lastname, id=id,
-                       home=home, mobile=mobile, work=work, phone2=phone2,
+                       home=home, work=work, mobile=mobile, phone2=phone2,
                        email=email, email2=email2, email3=email3)
 
     def get_contact_from_view_page(self, index):
--- test/test_phones.py
+++ test/test_phones.py
@@ -11,11 +11,13 @@ def test_phones_on_home_page(app):
 
 
 def test_phones_on_contact_view_page(app):
-    contact_from_view_page = app.contact.get_contact_from_view_page(0)
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    contacts = app.contact.get_contact_list()
+    index = randrange(len(contacts))
+    contact_from_view_page = app.contact.get_contact_from_view_page(index)
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
     assert contact_from_view_page.home == contact_from_edit_page.home
-    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
     assert contact_from_view_page.work == contact_from_edit_page.work
+    assert contact_from_view_page.mobile == contact_from_edit_page.mobile
     assert contact_from_view_page.phone2 == contact_from_edit_page.phone2
 
 
