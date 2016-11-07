6c6d9ef7ec815d99bb2f2bea7791b7052c2ec4c1
vatanov <atanov.v@gmail.com>
2016-11-07 21:55:22 +0200

    Merged tests in file test_contact_data.py

--- test/test_address.py
+++
@@ -1,9 +0,0 @@
-from random import randrange
-
-
-def test_address_on_home_page(app):
-    contacts = app.contact.get_contact_list()
-    index = randrange(len(contacts))
-    contact_from_home_page = app.contact.get_contact_list()[index]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(index)
-    assert contact_from_home_page.address == contact_from_edit_page.address
