f92146b022b9a49512701c7cf9a760d281724b78
vatanov <atanov.v@gmail.com>
2016-11-07 23:24:46 +0200

    "test_add_group.py" and "test_del_group.py" are modified
    Added asserts from DB

--- fixture/db.py
+++ fixture/db.py
@@ -1,4 +1,4 @@
-import pymysql.cursors
+import mysql.connector
 from model.group import Group
 
 class DbFixture:
@@ -8,7 +8,8 @@ class DbFixture:
         self.name = name
         self.user = user
         self.password = password
-        self.connection = pymysql.connect(host=host, database = name, user=user, password=password)
+        self.connection = mysql.connector.connect(host=host, database = name, user=user, password=password)
+        self.connection.autocommit = True
 
     def get_group_list(self):
         list = []
