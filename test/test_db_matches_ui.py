bf2a61ffe986c27d06767e263678c5fa7b5d3128
vatanov <atanov.v@gmail.com>
2016-11-09 22:19:14 +0200

    In "test_db_matches_ui.py" added test for contact

--- test/test_db_matches_ui.py
+++ test/test_db_matches_ui.py
@@ -1,10 +1,23 @@
 from model.group import Group
+from model.contact import Contact
 
 
 def test_group_list(app, db):
-    ui_list = app.group.get_group_list()
+    ui_group_list = app.group.get_group_list()
 
     def clean(group):
         return Group(id=group.id, name=group.name.strip())
-    db_list = map(clean, db.get_group_list())
-    assert sorted(ui_list, key = Group.id_or_max) == sorted(db_list, key = Group.id_or_max)
+    db_group_list = map(clean, db.get_group_list())
+    assert sorted(ui_group_list, key = Group.id_or_max) == sorted(db_group_list, key = Group.id_or_max)
+
+
+def test_contact_list(app, db):
+    ui_contact_list = app.contact.get_contact_list()
+
+    def clean(contact):
+        return Contact(id=contact.id, lastname=contact.lastname.strip(), firstname=contact.firstname.strip(),
+                     address=contact.address.strip(), email=contact.email.strip(), email2=contact.email.strip(),
+                     email3=contact.email3.strip(), home=contact.home.strip(), mobile=contact.mobile.strip(),
+                     work=contact.work.strip(), phone2=contact.phone2.strip())
+    db_contact_list = map(clean, db.get_contact_list())
+    assert sorted(ui_contact_list, key = Contact.id_or_max) == sorted(db_contact_list, key = Contact.id_or_max)
